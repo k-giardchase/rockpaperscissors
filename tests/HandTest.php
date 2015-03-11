9148f341ebaa4c86ebf778e49bb9b26a3b97962b
Kyle Giard-Chase & David Eliason <student@epicodus.com>
2015-03-11 10:24:23 -0700

    Change specs to test draw outcome first.

--- tests/HandTest.php
+++ tests/HandTest.php
@@ -7,10 +7,14 @@
         function test_rock_scissors()
         {
             //Arrange
+            $test_rock_scissors = new Hand;
+            $player1 = 'rock';
+            $player2 = 'scissors';
 
             //Act
-
+            $result = $test_rock_scissors->compareHand($player1, $player2);
             //Assert
+            $this->assertEquals("Player 1", $result);
 
         }
 
