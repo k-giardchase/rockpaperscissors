92e7966fcedf3f5b7b9fb77921f86c14ed75479d
Kyle Giard-Chase & David Eliason <student@epicodus.com>
2015-03-11 10:27:19 -0700

    Add a draw rock rock test

--- tests/HandTest.php
+++ tests/HandTest.php
@@ -4,19 +4,34 @@
 
     class HandTest extends PHPUnit_Framework_TestCase
     {
-        function test_rock_scissors()
+        function test_rock_rock()
         {
             //Arrange
-            $test_rock_scissors = new Hand;
+            $test_rock_rock = new Hand;
             $player1 = 'rock';
-            $player2 = 'scissors';
+            $player2 = 'rock';
 
             //Act
-            $result = $test_rock_scissors->compareHand($player1, $player2);
-            //Assert
-            $this->assertEquals("Player 1", $result);
+            $result = $test_rock_rock->compareHand($player1,$player2);
 
+            //Assert
+            $this->assertEquals("Draw", $result);
         }
+
+
+        // function test_rock_scissors()
+        // {
+        //     //Arrange
+        //     $test_rock_scissors = new Hand;
+        //     $player1 = 'rock';
+        //     $player2 = 'scissors';
+        //
+        //     //Act
+        //     $result = $test_rock_scissors->compareHand($player1, $player2);
+        //     //Assert
+        //     $this->assertEquals("Player 1", $result);
+        //
+        // }
 
 
     }
