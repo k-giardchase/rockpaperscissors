a632d47600952d58ae1c0ac75052456dba048ec2
Kyle Giard-Chase & David Eliason <student@epicodus.com>
2015-03-11 10:41:03 -0700

    Add functionality to pass player 1 win rock scissor test.

--- src/Hand.php
+++ src/Hand.php
@@ -4,9 +4,23 @@
     {
         function compareHand($player1, $player2)
         {
-            if( strtolower($player1) == 'rock' && strtolower($player2) == 'rock')
+
+            if(strtolower($player1) =="rock")
             {
-                return 'Draw';
+                if( strtolower($player2) == "rock")
+                {
+                    return "Draw";
+                }
+
+                elseif (strtolower($player2) == "scissors")
+                {
+                    return "Player 1";
+                }
+
+                elseif (strtolower($player2) == "paper")
+                {
+                    return "Player 2";
+                }
             }
         }
     }
